61ee066ae7a8360fe5de231f926e96b46fbf304a
Aiden McLendon <armclend@asu.edu>
2025-01-16 08:43:27 -0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'da65bf0d48msheed41979c1a04bep104388jsnd5079b844571',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4f847ac567209d674cc39281d888f55b";
